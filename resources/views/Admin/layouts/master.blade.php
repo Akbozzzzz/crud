07e8c97f80c75e7f7cd9ab7d24ddf4ea3ecde48c
Anand <anandkamboj002@gmail.com>
2024-01-27 15:03:25 +0530

    fixed error

--- resources/views/Admin/layouts/master.blade.php
+++ resources/views/Admin/layouts/master.blade.php
@@ -9,6 +9,7 @@
   <link rel="stylesheet" href="{{ asset('backend/assets/modules/bootstrap/css/bootstrap.min.css') }}">
   <link rel="stylesheet" href="{{ asset('backend/assets/modules/fontawesome/css/all.min.css') }}">
 
+
   <!-- CSS Libraries -->
   <link rel="stylesheet" href="{{ asset('backend/assets/modules/jqvmap/dist/jqvmap.min.css') }}">
   <link rel="stylesheet" href="{{ asset('backend/assets/modules/weather-icon/css/weather-icons.min.css') }}">
